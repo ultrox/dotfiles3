2df10439522e6fd81fd1a78150740211602fce38
Marko Vujanic <crashxx@gmail.com>
2022-01-03 00:38:35 +0100

    Create _env.zsh

---
+++ zsh/fzf/_env.zsh
@@ -0,0 +1,36 @@
+# Use ag to respect .gitignore
+export FZF_DEFAULT_COMMAND='ag -g ""'
+export FZF_ALT_C_COMMAND="bfs -type d -nohidden"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+LOC="$HOME/.tldr/cache/pages"
+
+# trigger it with tldr **
+_fzf_complete_tldr() {
+  _fzf_complete "--multi --reverse" "$@" < <(
+    find ~/.tldr/cache/pages/{linux,common} -type f -printf "%f\n"
+  )
+}
+
+_fzf_complete_tldr_post() {
+    sed 's/\.md//'
+}
+
+fzf-tldr-widget() {
+  local tldrmatch="$(find $LOC/{linux,common} -type f -printf "%f\n" \
+  | sed 's/\.md//' \
+  | FZF_DEFAULT_OPTS="
+     --height ${FZF_TMUX_HEIGHT:-40%} 
+     --reverse $FZF_DEFAULT_OPTS" fzf +m)"
+
+  if [[ -z "$tldrmatch" ]]; then
+    zle redisplay
+    return 0
+  fi
+  tldr "$tldrmatch"
+  local ret=$?
+  zle fzf-redraw-prompt
+  return $ret
+}
+zle     -N    fzf-tldr-widget
+bindkey '^O'  fzf-tldr-widget
